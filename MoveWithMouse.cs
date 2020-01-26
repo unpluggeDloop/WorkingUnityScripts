fc3e63844ed4b0da3be9c0fbc520ca4ed7b8d100
unpluggeDloop <unplugged.loop.mail@gmail.com>
2020-01-26 02:44:48 +0100

    Create MoveWithMouse.cs
    Add Component Box Collider 2D on object where you place script or object will not move.

---
+++ MoveWithMouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithMouse : MonoBehaviour
+{
+    private bool isDragging;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+    // Add Component Box Collider 2D on object where you place script
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
